c212fb44f1f99af36bf8eb7284af1eb4c65e3168
Robert Brown <robrown@opentext.com>
2017-01-05 14:23:04 -0500

    Edited for Jquery badges

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,15 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/rbrown68.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      var completedCourses = response.courses.completed;
+      var $badges = $('#badges');
+      for (i = 0; i < completedCourses.length; i++){
+        $badges.append("<div class=\'course\'><h3>" + completedCourses[i].title + "</h3><img src=\'" + completedCourses[i].badge + "\'>" + "<a href=\'" + completedCourses[i].url + "\' target=\'_blank\' class=\'btn btn-primary\' >See Course</a></div>");
+      }
+    }
+  });
 
 });
